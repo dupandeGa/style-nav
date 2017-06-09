2698688e482dfd0f147b421c57f43ca9d2b1ddf6
dupandeGa <shohibulroyan1@gmail.com>
2017-06-10 03:24:25 +0800

    Add files via upload

---
+++ Copyright.js
@@ -0,0 +1,9 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () {
+    if ($("#pink-footer,.pink-footer").attr("href") != "http://copyright-by-dupandes.blogspot.co.id/") {
+        window.location.href = "http://copyright-by-dupandes.blogspot.co.id/";
+    }
+});
+//]]>
+</script>
